862a0d03591a376fbad91144181739529606deca
danieldeandradelopes <danieldeandradelopes@gmail.com>
2020-03-01 02:10:50 -0300

    Added styles in project.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,28 @@
-import React, {Component} from 'react';
-import {Navbar, Nav, Button} from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 class Header extends Component {
-    state = {  }
-    render() {
-        return (
-            
-        );
-    }
+  state = {};
+
+  render() {
+    return (
+      <>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="">Movie Now</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto" style={{ marginLeft: '80%' }}>
+              <Nav>Welcome, Daniel! </Nav>
+            </Nav>
+
+            <Nav>
+              <Button variant="outline-danger">Sign Out</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
+  }
 }
 
 export default Header;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,54 +1,56 @@
 import React, { Component } from 'react';
-import { Container, Card, Row, Col, Button } from 'react-bootstrap';
+import './styles.scss';
+import { Container, Card, Row, Button } from 'react-bootstrap';
+import firebase from 'firebase';
+import Header from '../../components/Header';
 
 class Main extends Component {
-  state = {};
+  state = {
+    movies: [],
+  };
+
+  loadMovie = async () => {
+    const db = firebase.firestore();
+
+    db.collection('movies')
+      .orderBy('name')
+      .get()
+      .then(querySnapShot => {
+        querySnapShot.forEach(doc => {
+          this.setState({ movies: [...this.state.movies, doc.data()] });
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.loadMovie();
+  }
 
   render() {
     return (
       <>
-        <Container
-          style={{
-            paddingTop: 50,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            minWidth: 300,
-          }}
-        >
-          <h1 style={{ color: '#fff', fontFamily: 'ubuntu', marginTop: 20 }}>
-            Movie Now
-          </h1>
-          <Row style={{ display: 'flex', justifyContent: 'center' }}>
-            <Card style={{ width: 250, margin: 10 }}>
-              <Card.Img
-                variant="top"
-                style={{
-                  width: 220,
-                  height: 320,
-                  alignSelf: 'center',
-                  paddingTop: 20,
-                  cursor: 'pointer',
-                }}
-                src="https://media.fstatic.com/DXGiOJQmIXVEvACtLSG5MiPsZaA=/fit-in/290x478/smart/media/movies/covers/2012/07/56eb1ab9acd1a73c6552056b421fc5ed.jpg"
-              />
-              <Card.Body
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text style={{ fontSize: 12 }}>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the cards content.
-                </Card.Text>
-                <Button style={{ width: '100%' }} variant="outline-dark">
-                  Go somewhere
-                </Button>
-              </Card.Body>
-            </Card>
+        <Header />
+        <Container className="container">
+          <h1>Movie Now</h1>
+          <Row>
+            {this.state.movies.map(m => (
+              <Card className="card">
+                <Card.Img variant="top" src={m.url_picture} />
+                <Card.Body>
+                  <Card.Title
+                    style={{
+                      fontSize: 18,
+                      fontWeight: 'bold',
+                      textAlign: 'center',
+                    }}
+                  >
+                    {m.name}
+                  </Card.Title>
+                  <Card.Text>{m.description}</Card.Text>
+                  <Button variant="outline-dark">Watch</Button>
+                </Card.Body>
+              </Card>
+            ))}
           </Row>
         </Container>
       </>
